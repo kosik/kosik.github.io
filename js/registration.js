8ad70aaf2e0699f30ea3b4febe8441b0d67da4cd
Nick Malezh <n.malezh@gmail.com>
2017-10-20 15:26:19 +0300

    registration implemented; without selection styles

--- js/registration.js
+++ js/registration.js
@@ -1,5 +1,21 @@
 ;(function(){
 	'use strict';
 
-    
+    $('#registration #_to-step-2').on('click', function(){
+        $('._steps-container ._step').removeClass('active')
+        $('._step-title').text('Заполните следующие поля:')
+        $('._steps-container ._step-2').addClass('active')
+    })
+
+    $('#registration #_to-login').on('click', function(){
+        $('._steps-container ._step').removeClass('active')
+        $('._window-title').text('Вход')
+        $('._step-title').text('Введите данные для входа:')
+        $('._steps-container ._step-login').addClass('active')
+    })
+
+    $('#registration ._select-account-type').on('click', function(){
+        $('._account-type').removeClass('active')
+        $('._account-type._' + $(this).attr('type')).addClass('active')
+    })
 })();
